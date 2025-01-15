ab1b569977c2feda89619e0395393e334123b560
sumaya257 <sumayaece19@gmai.com>
2025-01-15 22:05:00 +0600

    creation-of-post-methods-fot-storing-user-data

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ju1bs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,14 +24,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const userCollection = client.db('petAdoption').collection('users')
+
+
+    //users related api
+    app.post('/users',async(req,res)=>{
+        const user = req.body
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+    })
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
